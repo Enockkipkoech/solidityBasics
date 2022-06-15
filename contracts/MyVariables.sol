202064e7a780227d93f81f8b2c1f1ddad31f45c8
enockkipkoech19@gmail.com <enockkipkoech19@gmail.com>
2022-06-15 13:13:41 +0300

    :star2: Sol Variables and accessibility

--- contracts/MyVariables.sol
+++ contracts/MyVariables.sol
@@ -4,17 +4,20 @@ pragma solidity ^0.8.0;
 
 contract MyVariables {
     /// state variables
-    uint256 number = 20;
-    bool paused = true;
-    uint256 time = block.timestamp; /// global variables
+    uint256 public number = 20;
+    bool private paused = true;
+    uint256 internal time = block.timestamp; /// global variables
 
     function myFunc() public {
         /// Local Variables
         uint256 localNumber = 20;
         bool localPause = true;
-
         /// global variables
         address theSenderAddress = msg.sender;
-        msg.value
     }
+}
+
+contract INeedVariables is MyVariables {
+    uint256 public theAwesomeVar = 200;
+    uint256 public theTime = time;
 }
